1c920a200708854c813c08492f418bf7f492b1da
Omar Sosa Rodriguez <omarfsosa@gmail.com>
2019-10-01 07:22:52 +0100

    initial commit

---
+++ separation_mvp.py
@@ -0,0 +1,59 @@
+from scipy.optimize import minimize
+from sklearn.metrics import roc_curve, recall_score
+import pandas as pd
+import numpy as np
+
+from utils import check_lengths, check_binaries
+
+
+class SeparatedClassifier:
+    def __init__(self, y_train, R_train, A_train):
+        check_lengths(y_train, R_train, A_train)
+        check_binaries(y_train)
+        self.y_train = np.array(y_train)
+        self.R_train = np.array(R_train)
+        self.A_train = np.array(A_train)
+
+    def fit(self, goal_fpr, goal_tpr):
+        groups = np.unique(self.A_train)
+        self.randomized_thresholds = dict()
+        for g in groups:
+            y_true_g = self.y_train[self.A_train == g]
+            R_g = self.R_train[self.A_train == g]
+            self.randomized_thresholds[g] = self._find_thresholds_and_probas(
+                y_true_g,
+                R_g,
+                goal_fpr,
+                goal_tpr,
+            )
+
+    def fair_predict(self, R_test, A_test):
+        groups = np.unique(A_test)
+        y_pred = []
+        for r, g in zip(R_test, A_test):
+            *a, p = self.randomized_thresholds[g]
+            t = np.random.choice(a, p=[1 - p , p])
+            y_pred.append(1 if r >= t else 0)
+
+        return np.array(y_pred)
+
+    def _find_thresholds_and_probas(self, y_true, scores, goal_fpr, goal_tpr):
+        fpr, tpr, thresholds = roc_curve(y_true, scores, drop_intermediate=False)
+
+        def distance(d3array):
+            t0, t1, p = d3array
+            i = np.argmin(abs(t0 - thresholds))
+            j = np.argmin(abs(t1 - thresholds))
+            x = fpr[i] + p * (fpr[j] - fpr[i])
+            y = tpr[i] + p * (tpr[j] - tpr[i])
+            return np.hypot(x - goal_fpr, y - goal_tpr)
+
+        initial_guess = np.array([0.3, 0.5, .5])
+        res = minimize(
+            distance,
+            initial_guess,
+            method='nelder-mead',
+            options={'xtol': 1e-10},
+        )
+        t0, t1, p = res["x"]
+        return t0, t1, p
---
+++ utils.py
@@ -0,0 +1,117 @@
+import numpy as np
+
+
+def check_lengths(*arrays):
+    """
+    Check that all arrays have the same lenght.
+
+    Parameters
+    ----------
+    *arrays : list or tuple of input objects.
+        Objects that will be checked for consistent length.
+    """
+    lengths = [len(X) for X in arrays if X is not None]
+    uniques = np.unique(lengths)
+    if len(uniques) > 1:
+        message = "Input arrays should all be the same length."
+        raise ValueError(message)
+
+def check_binaries(*arrays):
+    """
+    Check that all values in the arrays are 0s or 1s.
+
+    Parameters
+    ----------
+    *arrays : list or tuple of input objects.
+        Objects that will be checked for consistent length.
+    """
+    values = [set(X) for X in arrays if X is not None]
+    all_valid = all(v.issubset({0, 1}) for v in values)
+    if not all_valid:
+        message = "Input arrays should only contain 0s and/or 1s."
+        raise ValueError(message)
+
+
+def tp_rate(y_true, y_pred) -> float:
+    """
+    True positive rate.
+
+    Parameters
+    ----------
+    y_true : 1d array-like of binaries
+        Ground truth (correct) target values.
+
+    y_pred : 1d array-like of binaries
+        Estimated targets as returned by a classifier.
+    """
+    check_lengths(y_true, y_pred)
+    check_binaries(y_true, y_pred)
+    y_true = np.array(y_true)
+    y_pred = np.array(y_pred)
+    if all(y_true == 0):
+        return np.nan
+
+    rate = (y_true @ y_pred) / (y_true @ y_true)
+    return rate
+
+def fp_rate(y_true, y_pred) -> float:
+    """
+    False positive rate.
+
+    Parameters
+    ----------
+    y_true : 1d array-like of binaries
+        Ground truth (correct) target values.
+
+    y_pred : 1d array-like of binaries
+        Estimated targets as returned by a classifier.
+    """
+    check_lengths(y_true, y_pred)
+    check_binaries(y_true, y_pred)
+    y_false = 1 - np.array(y_true)
+    y_pred = np.array(y_pred)
+    if all(y_false == 0):
+        return np.nan
+
+    rate = (y_false @ (y_pred)) / (y_false @ y_false)
+    return rate
+
+
+def classification_report(y_true, y_pred, A) -> str:
+    """
+    String showing the true positive rate and false
+    positive rate for each group.
+
+    Parameters
+    ----------
+    y_true : 1d array-like of binaries
+        Ground truth (correct) target values.
+
+    y_pred : 1d array-like of binaries
+        Estimated targets as returned by a classifier.
+
+    A: 1d array like
+        Labels for the different groups.
+
+    Returns
+    -------
+    report: str
+    """
+    check_lengths(y_true, y_pred, A)
+    check_binaries(y_true, y_pred)
+    groups = np.unique(A)
+    header = "{:<4}{:^6}{:^6}".format("A", "TPR", "FPR")
+    row_fmt = "{:<4}{:^6.2f}{:^6.2f}"
+    lines = [header, "-" * len(header)]
+    for g in groups:
+        y_true_g = y_true[A == g]
+        y_pred_g = y_pred[A == g]
+        tpr_g = tp_rate(y_true_g, y_pred_g)
+        fpr_g = fp_rate(y_true_g, y_pred_g)
+        lines.append(row_fmt.format(g, tpr_g, fpr_g))
+
+    tpr = tp_rate(y_true, y_pred)
+    fpr = fp_rate(y_true, y_pred)
+    lines.append(row_fmt.format("All", tpr, fpr))
+    report = "\n".join(lines)
+    return report
